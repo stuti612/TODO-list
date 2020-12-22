cfca16a08cb9f553c8a95160cba826774fcb5368
stuti612 <68732415+stuti612@users.noreply.github.com>
2020-12-22 15:35:39 +0530

    Added my project

---
+++ assets/js/todos.js
@@ -0,0 +1,25 @@
+//Check off specific todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type = 'text']").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		//creating a new li and add to ul
+		$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText +"</li>");
+	}
+});
+
+$(".fa-pencil-alt").click(function(){
+	$("input[type = 'text']").fadeToggle();
+});
